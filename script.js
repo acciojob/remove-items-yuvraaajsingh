ed71e12c6a1c944faa33342866e4dc21dd1a72ba
Yuvraj Singh <84833138+yuvraaajsingh@users.noreply.github.com>
2025-05-19 13:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function remove(){
+	let color= document.getElementById("colorSelect");
+	let selectedIndex= color.selectedIndex;
+
+	if(selectedIndex>=0)
+	{
+			color.remove(selectedIndex);
+	}
+}
